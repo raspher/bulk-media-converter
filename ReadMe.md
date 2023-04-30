bd3ece514db4edf7df2e16b84548a616b6640c22
Szymon Scholz <raspher@posteo.me>
2023-04-30 14:08:13 +0200

    fixes #1 #2 #4

--- ReadMe.md
+++ ReadMe.md
@@ -2,11 +2,16 @@
 Just an ffmpeg overlay which converts media directory keeping it's structure
 
 ## Dependencies
+ - bash
  - ffmpeg
  - coreutils
 
 # Usage
+```bash
 bmc.sh [input_dir] [output_dir]
+```
 
 It scans for files in input dir, then converts them and put to output dir while preserving file structure.
 
+# Contribution
+PR's are welvome as well as i'm open to co-op on this project (moving to organization repo)
--- bmc.sh
+++ bmc.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 2 ] ; then
-    usage
-    exit
-fi
+supportedExtensions="\.webm$|\.flv$|\.vob$|\.ogg$|\.ogv$|\.drc$|\.gifv$|\.mng$|\.avi$|\.mov$|\.qt$|\.wmv$|\.yuv$|\.rm$|\.rmvb$|/.asf$|\.amv$|\.mp4$|\.m4v$|\.mp*$|\.m?v$|\.svi$|\.3gp$|\.flv$|\.f4v$"
 
 function getDestination {
     local lenght=${#1}
@@ -12,7 +9,7 @@ function getDestination {
 }
 
 function getSources {
-    echo $(find $1 -type f -printf '%p:')
+    echo "$(find "$1" -type f | grep -E "$supportedExtensions" |  tr '\n' ':')"
 }
 
 function convert {
@@ -23,18 +20,23 @@ function usage {
     echo "Usage: [input_directory] [output_directory]"
 }
 
+if [ $# -ne 2 ] ; then
+    usage
+    exit
+fi
+
 filesFrom=$(realpath "$1")
 filesTo=$(realpath "$2")
 
-sources=$(getSources $1)
+sources=$(getSources "$filesFrom")
 
 while read line
 do
-    absPath=$(realpath "$line")
+    absPath=$(realpath "${line}")
     fileName=$filesTo${absPath:${#filesFrom}}
     dirName="$(dirname "$fileName")"
     destination="${fileName%.*}.mp4"
     mkdir -p "$dirName"
     echo "Converting $absPath ---> $destination"
     convert "$absPath" "$destination"
-done < <(echo $sources | tr ':' '\n')
+done < <(echo "$sources" | tr ':' '\n')
